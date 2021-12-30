3e34cf1fa0a566e0cee2603818f86dff03587b39
Omar Sumon <73730920+worstfrontendcoder@users.noreply.github.com>
2021-12-30 05:13:55 -0600

    add highlighted mouse function

--- code/script.js
+++ code/script.js
@@ -10,6 +10,22 @@ const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
 
+// mouse
+const mouse = {
+    x: 10,
+    y: 10,
+    width: 0.1,
+    height: 0.1,
+}
+let canvasPosition = canvas.getBoundingClientRect();
+canvas.addEventListener('mousemove', function(e){
+    mouse.x = e.x - canvasPosition.left;
+    mouse.y = e.y - canvasPosition.top;
+});
+canvas.addEventListener('mouseleave', function(){
+    mouse.x = undefined;
+    mouse.y = undefined;
+});
 // game board
 const controlsBar = {
     width: canvas.width,
@@ -23,8 +39,11 @@ class Cell {
         this.height = cellSize;
     }
     draw() {
-        ctx.strokeStyle = 'black';
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        if (mouse.x && mouse.y && collision(this, mouse)) {
+            ctx.strokeStyle = 'black';
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
+        }
+        
     }
 }
 function createGrid() {
@@ -46,6 +65,7 @@ function handleGameGrid() {
 // resources
 // utilities
 function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'blue';
     ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
     handleGameGrid();
@@ -53,3 +73,12 @@ function animate() {
 }
 
 animate();
+
+function collision(first, second){
+    if ( !( first.x > second.x + second.width ||
+            first.x + first.width < second.x ||
+            first.y > second.y + second.height ||
+            first.y + first.height < second.y ) ) {
+        return true;
+    }
+}
